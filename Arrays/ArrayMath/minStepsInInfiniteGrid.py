10fd215f05d75b9fcd697259e77764a6d1aea427
juneja <ajuneja1008@gmail.com>
2021-04-05 16:16:53 +0530

    minStepsInInfiniteGrid.py

---
+++ Arrays/ArrayMath/minStepsInInfiniteGrid.py
@@ -0,0 +1,12 @@
+# The minimum steps required is the maximum absolute value of X,Y coordinates
+
+class Solution:
+	# @param A : list of integers
+	# @param B : list of integers
+	# @return an integer
+	def coverPoints(self, A, B):
+	    m=0
+	    for i in range (1,len(A)):
+	        m += max([abs(A[i] - A[i-1]), abs(B[i] - B[i-1])])
+	    return m
+
